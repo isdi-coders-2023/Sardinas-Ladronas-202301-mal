bcd8d99bd34a54cb534e9fb9fffbd8278716f006
Lara-Vel <lara.vel@outlook.com>
2023-02-14 20:15:31 +0100

    changes in the app to avoid conflict

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,14 @@
 import React from 'react';
 import './App.css';
+import './components/NavBar/NavBar.css';
+import NavBar from './components/NavBar/NavBar';
 
 function App() {
-  return <div className="app-container"></div>;
+  return (
+    <div className="app-container">
+      <NavBar />
+    </div>
+  );
 }
 
 export default App;
