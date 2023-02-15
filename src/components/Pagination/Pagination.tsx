aba46836d1f3dcf5a37b4746cb35fe5020aea29f
Lara-Vel <lara.vel@outlook.com>
2023-02-15 21:25:46 +0100

    add pagination components

---
+++ src/components/Pagination/Pagination.tsx
@@ -0,0 +1,47 @@
+import './Pagination.css';
+
+const alphabetLetters = [
+  'A',
+  'B',
+  'C',
+  'D',
+  'E',
+  'F',
+  'G',
+  'H',
+  'I',
+  'J',
+  'K',
+  'L',
+  'M',
+  'N',
+  'Ñ',
+  'O',
+  'P',
+  'Q',
+  'R',
+  'S',
+  'T',
+  'U',
+  'V',
+  'W',
+  'X',
+  'Y',
+  'Z',
+];
+
+const Pagination = () => {
+  return (
+    <div className="pagination">
+      <ul className="pagination__letters">
+        {alphabetLetters.map((letter) => (
+          <li key={letter} className="pagination__letter">
+            <a href={`/${letter}`}>{letter}</a>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default Pagination;
