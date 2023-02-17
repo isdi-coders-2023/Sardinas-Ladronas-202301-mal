9d019031a2de76536d9d2d3c3946e56e30f054dc
`Enrique` <`e.jimenezmoreno@gmail.com`>
2023-02-17 16:17:40 +0100

    Remove console.log

--- src/store/types/Action.ts
+++ src/store/types/Action.ts
@@ -1,12 +1,15 @@
 import { Recipe } from '../../models/recipe.r';
 
 export enum ActionTypes {
-  GET_RECIPES_BY_LETTER = 'listRecipes',
+  ADD_RECIPES_BY_LETTER = 'addRecipes',
+  LIST_RECIPES_BY_LETTER = 'listRecipes',
 }
 
-type RecipeAction = {
-  type: ActionTypes.GET_RECIPES_BY_LETTER;
-  payload: Recipe[];
-};
+type RecipeAction =
+  | {
+      type: ActionTypes.ADD_RECIPES_BY_LETTER;
+      payload: Recipe[];
+    }
+  | { type: ActionTypes.LIST_RECIPES_BY_LETTER; payload: Recipe[] };
 
 export default RecipeAction;
