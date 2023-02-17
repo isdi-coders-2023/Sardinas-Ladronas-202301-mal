cafce770e87d01083a8a96301c0695ea9867c3fe
Adrian_G <adriangarcia.comunicacion@gmail.com>
2023-02-17 13:33:14 +0100

    Fix test description

--- src/components/DetailInstructions/DetailInstructions.test.tsx
+++ src/components/DetailInstructions/DetailInstructions.test.tsx
@@ -3,7 +3,7 @@ import { meal } from '../../mocks/mealMock';
 import DetailInstructions from './DetailInstructions';
 
 describe('Given a recipe card component', () => {
-  test('When card is rendered, it should show a recipe img', () => {
+  test('When card is rendered, it should show instructions, category and area with images', () => {
     render(<DetailInstructions recipe={meal.meals[0]} />);
     const instructionsElement = screen.getAllByRole('heading');
     const imgElements = screen.getAllByRole('img');
