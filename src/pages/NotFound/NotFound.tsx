6d3ac20cd671ed00e6629836d7118e7dd7e0ef25
Lara-Vel <lara.vel@outlook.com>
2023-02-18 23:27:08 +0100

    add elements to the page 404

--- src/pages/NotFound/NotFound.tsx
+++ src/pages/NotFound/NotFound.tsx
@@ -1,9 +1,16 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import './NotFound.css';
 
 const NotFound = () => {
   return (
-    <div>
-      <h1>404 NotFound</h1>
+    <div className="page-error">
+      <h1>Error 404</h1>
+      <p>Page not found</p>
+      <img src="/assets/img/error-404.svg" alt="page error 404" />
+      <Link data-testid="link" to={'/'}>
+        Go home
+      </Link>{' '}
     </div>
   );
 };
