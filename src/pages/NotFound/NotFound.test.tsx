0bb57508264e9d732fa55ae95fec52276e99998a
Lara-Vel <lara.vel@outlook.com>
2023-02-18 23:28:56 +0100

    add test 100% to the page 404

--- src/pages/NotFound/NotFound.test.tsx
+++ src/pages/NotFound/NotFound.test.tsx
@@ -1,7 +1,17 @@
-import { render } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
+import { MemoryRouter } from 'react-router-dom';
 import NotFound from './NotFound';
 
-test('render page', () => {
-  render(<NotFound />);
-  expect(true).toBe(true);
+describe('Given a page error 404', () => {
+  test('When rendered, Then it should show all information of the page', () => {
+    render(<NotFound />, { wrapper: MemoryRouter });
+    const titleElement = screen.getByText('Error 404');
+    const paragraphElement = screen.getByText('Page not found');
+    const imageElement = screen.getByRole('img');
+    const linkElement = screen.getByRole('link');
+    expect(titleElement).toBeInTheDocument();
+    expect(paragraphElement).toBeInTheDocument();
+    expect(imageElement).toBeInTheDocument();
+    expect(linkElement).toBeInTheDocument();
+  });
 });
