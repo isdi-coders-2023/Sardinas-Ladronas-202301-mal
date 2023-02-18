51d714181356365a003f9467696ef0fbb283d65c
Adrian_G <adriangarcia.comunicacion@gmail.com>
2023-02-18 15:36:47 +0100

    Fix DetailHeader test

--- src/components/DetailHeader/DetailHeader.test.tsx
+++ src/components/DetailHeader/DetailHeader.test.tsx
@@ -1,16 +1,14 @@
-// import { render, screen } from '@testing-library/react';
-// import DetailHeader from './DetailHeader';
-// import { meal } from '../../mocks/mealMock';
+import { render, screen } from '@testing-library/react';
+import DetailHeader from './DetailHeader';
+import { meal } from '../../mocks/mealMock';
 
-// describe('Given a recipe card component', () => {
-//   test('When card is rendered, it should show a recipe img', () => {
-//     render(<DetailHeader recipe={meal.meals[0]} />);
-//     const imgElement = screen.getByAltText('recipe img');
-//     const titleElement = screen.getByRole('heading');
+describe('Given a recipe card component', () => {
+  test('When card is rendered, it should show a recipe img', () => {
+    render(<DetailHeader recipes={meal.meals} />);
+    const imgElement = screen.getByAltText('recipe img');
+    const titleElement = screen.getByRole('heading');
 
-//     expect(imgElement).toBeInTheDocument();
-//     expect(titleElement).toBeInTheDocument();
-//   });
-// });
-
-export {};
+    expect(imgElement).toBeInTheDocument();
+    expect(titleElement).toBeInTheDocument();
+  });
+});
