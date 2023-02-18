e1114f192061c882b04c25547540adfacf732f5f
Adrian_G <adriangarcia.comunicacion@gmail.com>
2023-02-18 11:09:01 +0100

    Add new hook for implementing detail page

---
+++ src/hooks/useGetDetail/useGetDetail.ts
@@ -0,0 +1,22 @@
+import { useCallback, useContext } from 'react';
+import { useParams } from 'react-router-dom';
+import RecipeContext from '../../store/context/recipes.context';
+import { ActionTypes } from '../../store/types/Action';
+
+export default function useGetDetail() {
+  const { recipes, dispatch } = useContext(RecipeContext);
+  const { id } = useParams();
+  const getDetail = useCallback(async () => {
+    const response = await fetch(
+      `https://www.themealdb.com/api/json/v1/1/lookup.php?i=${id ?? '52772'}`
+    );
+    const recipesResponse = await response.json();
+
+    const recipeAction = {
+      type: ActionTypes.GET_RECIPE_DETAIL,
+      payload: recipesResponse?.meals,
+    };
+    dispatch(recipeAction);
+  }, [dispatch, id]);
+  return { recipes, getDetail };
+}
