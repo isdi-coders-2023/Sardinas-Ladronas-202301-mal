6d2041908b16c3579c54bfa14037340b20d8f1b1
Lara-Vel <lara.vel@outlook.com>
2023-02-19 19:40:44 +0100

    add Loader component with funcionality,style and css

---
+++ src/components/Loader/Loader.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import './Loader.css';
+
+const Loader = () => {
+  return (
+    <div data-testid="spinner" className="spinner">
+      <div className="lds-spinner">
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+      </div>
+    </div>
+  );
+};
+
+export default Loader;
